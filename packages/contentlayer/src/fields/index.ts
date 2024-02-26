263ebfa2bdd6bf2bfd691f63e0ff520387bc28d9
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-02-26 21:33:34 +0100

    feat(@galactiks/contentlayer): export contentlayer fields

---
+++ packages/contentlayer/src/fields/index.ts
@@ -0,0 +1,12 @@
+export * from './core.js';
+
+export * from './article.js';
+export * from './creative-work.js';
+export * from './offer.js';
+export * from './organization.js';
+export * from './person.js';
+export * from './place.js';
+export * from './product.js';
+export * from './webpage-element.js';
+export * from './website.js';
+export * from './page.js';
