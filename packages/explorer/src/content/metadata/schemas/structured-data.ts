0b8fc4084ca2305ae2557ba1b772eed0c6f322a4
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-02-29 11:48:34 +0100

    feat(@galactiks/explorer): add product json schema

--- packages/explorer/src/content/metadata/schemas/structured-data.ts
+++ packages/explorer/src/content/metadata/schemas/structured-data.ts
@@ -1,14 +1,21 @@
 import type { Thing, WithContext } from 'schema-dts';
 
-import type { Content } from '../../../types/index.js';
+import type { Content, ContentlayerProduct } from '../../../types/index.js';
 import { getArticle } from './article.js';
+import { getProduct } from './product.js';
 import { getBreadcrumb } from './breadcrumb.js';
 
 export const getStructuredDataSchemas = (document: Content) => {
   let schemas: WithContext<Thing>[] = [getBreadcrumb(document)];
 
-  if (document.type === 'Article') {
-    schemas = schemas.concat(getArticle(document));
+  switch (document.type) {
+    case 'Article':
+      schemas = schemas.concat(getArticle(document));
+      break;
+
+    case 'Product':
+      schemas = schemas.concat(getProduct(document as unknown as ContentlayerProduct));
+      break;
   }
 
   return schemas;
